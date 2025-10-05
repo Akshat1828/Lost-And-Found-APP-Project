7e67bbce1f35b2e6ae2b8c25f16be1c60197fbf4
ARTLEST <atharvakumarseth251206@gmail.com>
2025-10-06 01:08:04 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
